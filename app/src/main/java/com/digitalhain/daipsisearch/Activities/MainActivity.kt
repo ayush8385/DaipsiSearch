259b7c9bb9841bdb001f5b8350feeb70409f7d9a
Ayush <am838578@gmail.com>
2021-06-06 14:02:37 +0530

    Converted Java to kotlin MainActivity.kt

--- app/src/main/java/com/digitalhain/daipsisearch/Activities/MainActivity.kt
+++ app/src/main/java/com/digitalhain/daipsisearch/Activities/MainActivity.kt
@@ -1,59 +1,35 @@
-package com.digitalhain.daipsisearch.Activities;
+package com.digitalhain.daipsisearch.Activities
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.RadioGroup
+import android.os.Bundle
+import com.digitalhain.daipsisearch.R
+import android.widget.RadioButton
+import android.content.Intent
+import android.util.Log
+import android.view.View
+import com.digitalhain.daipsisearch.Activities.searchedItemActivity
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
-
-import com.digitalhain.daipsisearch.R;
-
-public class MainActivity extends AppCompatActivity {
-
-    private RadioGroup radioGroup;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        radioGroup = (RadioGroup) findViewById(R.id.groupradio);
-
-
-        radioGroup.clearCheck();
-        radioGroup.setOnCheckedChangeListener(
-                new RadioGroup.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(RadioGroup group, int checkedId) {
-
-                        RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
-
-                        switch (checkedId) {
-                            case R.id.radia_eng:
-                                Log.d("RADIO_BTN", "radia_eng Clicked");
-                                break;
-                            case R.id.radia_med:
-                                Log.d("RADIO_BTN", "radia_med Clicked");
-                                break;
-                            case R.id.radia_comm:
-                                Log.d("RADIO_BTN", "radia_comm Clicked");
-                                break;
-                            case R.id.radia_gvt_exam:
-                                Log.d("RADIO_BTN", "radia_gvt_exam Clicked");
-                                break;
-                        }
-                    }
-                });
+class MainActivity : AppCompatActivity() {
+    private var radioGroup: RadioGroup? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        radioGroup = findViewById<View>(R.id.groupradio) as RadioGroup
+        radioGroup!!.clearCheck()
+        radioGroup!!.setOnCheckedChangeListener { group, checkedId ->
+            val radioButton = group.findViewById<View>(checkedId) as RadioButton
+            when (checkedId) {
+                R.id.radia_eng -> Log.d("RADIO_BTN", "radia_eng Clicked")
+                R.id.radia_med -> Log.d("RADIO_BTN", "radia_med Clicked")
+                R.id.radia_comm -> Log.d("RADIO_BTN", "radia_comm Clicked")
+                R.id.radia_gvt_exam -> Log.d("RADIO_BTN", "radia_gvt_exam Clicked")
+            }
+        }
     }
 
-    public void gotoNext(View view) {
-        Intent intent = new Intent(MainActivity.this, searchedItemActivity.class);
-        startActivity(intent);
+    fun gotoNext(view: View?) {
+        val intent = Intent(this@MainActivity, searchedItemActivity::class.java)
+        startActivity(intent)
     }
 }
