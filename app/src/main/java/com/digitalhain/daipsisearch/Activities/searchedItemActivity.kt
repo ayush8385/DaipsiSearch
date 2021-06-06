01d9d3287b929dd2355182b3a7545949c921d66e
Ayush <am838578@gmail.com>
2021-06-06 17:25:07 +0530

    Updates url http to https

--- app/src/main/java/com/digitalhain/daipsisearch/Activities/searchedItemActivity.kt
+++ app/src/main/java/com/digitalhain/daipsisearch/Activities/searchedItemActivity.kt
@@ -1,8 +1,11 @@
 package com.digitalhain.daipsisearch.Activities
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
@@ -21,51 +24,63 @@ class searchedItemActivity : AppCompatActivity() {
     lateinit var recyclerAdapter:SearchAdapter
     lateinit var layoutManager: RecyclerView.LayoutManager
     var subjectArray = arrayListOf<com.digitalhain.daipsisearch.Activities.Subject>()
+    lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_searched_item)
 
-        val url="http://daipsi.co.in/api/find-qna.php"
+        progressBar=findViewById(R.id.progressbar)
+        progressBar.visibility= View.VISIBLE
+
+        val url="https://daipsi.com/api/find-qna.php"
         val queue= Volley.newRequestQueue(this)
 
         recyclerView=findViewById(R.id.recyclermain)
         layoutManager= LinearLayoutManager(this)
 
-            val stringRequest = StringRequest(
-                Request.Method.GET, url,
-                { response ->
-                    try {
-                        //converting the string to json array object
-                        val array = JSONArray(response)
+            if(ConnectionManager().checkconnectivity(this)){
+                val stringRequest = StringRequest(
+                    Request.Method.GET, url,
+                    { response ->
+                        Log.d("HEloooo,...","Hellll")
+                        try {
+                            //converting the string to json array object
+                            val array = JSONArray(response)
 
-                        //traversing through all the object
-                        for (i in 0 until array.length()) {
 
-                            //getting product object from json array
-                            val product = array.getJSONObject(i)
+                            //traversing through all the object
+                            for (i in 0 until array.length()) {
 
-                            val itemjsonRequest=com.digitalhain.daipsisearch.Activities.Subject(
-                                product.getString("Question"),
-                                product.getString("Answer")
-                            )
-                            subjectArray.add(itemjsonRequest)
+                                //getting product object from json array
+                                val product = array.getJSONObject(i)
 
-                            //adding the product to product list
+                                val itemjsonRequest=com.digitalhain.daipsisearch.Activities.Subject(
+                                    product.getString("Question"),
+                                    product.getString("Answer")
+                                )
+                                subjectArray.add(itemjsonRequest)
+
+                                //adding the product to product list
+                            }
+
+                            recyclerAdapter= SearchAdapter(this,subjectArray)
+
+                            recyclerView.layoutManager=layoutManager
+                            recyclerView.adapter=recyclerAdapter
+                            progressBar.visibility=View.GONE
+                            //creating adapter object and setting it to recyclerview
+
+
+                        } catch (e: JSONException) {
+                            Toast.makeText(applicationContext,"Error found", Toast.LENGTH_LONG).show()
                         }
-
-                        recyclerAdapter= SearchAdapter(this,subjectArray)
-
-                        recyclerView.layoutManager=layoutManager
-                        recyclerView.adapter=recyclerAdapter
-                        //creating adapter object and setting it to recyclerview
-
-
-                    } catch (e: JSONException) {
-                        Toast.makeText(applicationContext,"Error found", Toast.LENGTH_LONG).show()
                     }
-                }
-            ) { }
-            queue.add(stringRequest)
+                ) { }
+                queue.add(stringRequest)
+            }
+           else{
+                Toast.makeText(applicationContext,"Not connected to internet", Toast.LENGTH_LONG).show()
+            }
 
 
     }
